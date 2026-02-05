1c97e088df92b6f43e3db13a5825395c79588194
Jfdeev <jfrsoares161205@gmail.com>
2026-02-05 15:27:43 -0300

    feat: create tool executor and add langchain tavily

--- src/chains/ActorChain.py
+++ src/chains/ActorChain.py
@@ -7,10 +7,11 @@ from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_ollama import ChatOllama
-from ..schemas.schemas import AnswerQuestion
+from ..schemas.schemas import AnswerQuestion, RevisedAnswer
 
 llm = ChatOllama(model="llama3.1:8b", format="json", temperature=0)
 parser_pydantic = PydanticOutputParser(pydantic_object=AnswerQuestion)
+parser_pydantic_revised = PydanticOutputParser(pydantic_object=RevisedAnswer)
 
 actor_prompt_template = ChatPromptTemplate.from_messages(
     [
@@ -47,6 +48,19 @@ first_responder_prompt_template = actor_prompt_template.partial(
 )
 
 first_responder = first_responder_prompt_template | llm | parser_pydantic
+
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
+
+revisor = revise_instructions | llm | parser_pydantic_revised
+
+
 
 if __name__ == "__main__":
     human_message = HumanMessage(
--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -9,8 +9,15 @@ class Reflection(BaseModel):
 
 
 class AnswerQuestion(BaseModel):
+    """Answer the qusetion"""
     answer: str = Field(description="~250 word detailed answer to the question")
     reflection: Reflection = Field(description="your reflection on the initial answer")
     search_queries: List[str] = Field(
         description="1-3 search queries for researching improvements to address the critique of your current answer"
+    )
+
+class RevisedAnswer(BaseModel):
+    """Revise your original answer to the question"""
+    references: List[str] = Field(
+        description="Citations motivating your updated answer"
     )
